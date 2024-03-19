5c4590c0a24064a98af85de5bcbdb60221c8d1e8
Richard Sithole <sithole@optimal-systems.de>
2024-03-19 09:02:23 +0100

    Updates

--- js/main.js
+++ js/main.js
@@ -1,4 +1,4 @@
-﻿import * as lib from "./library/library.js";
+﻿import * as lib from '../../node_modules/@enaio-client/communication-library/dist/module.js';
 
 let currentSelectedObjects = [];
 let dashletName = "Dashlet";
